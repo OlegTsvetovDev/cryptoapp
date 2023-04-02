d4e0096e2ddcf35f4b8e68fbfe3736f8653d0ac1
Oleg Tsvetov <brainstormmedia@ya.ru>
2023-04-02 21:27:32 +0500

    Routing changes. Added Exchangedeatials.

--- src/components/Exchanges/index.jsx
+++ src/components/Exchanges/index.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Col, Typography, Collapse } from "antd";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
 
+const { Title, Text } = Typography;
+const { Panel } = Collapse;
+const text = "loremaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 const Exchanges = () => {
-  return  (
-    <div>
-      Exchanges
-    </div>
-  )
-}
+  const { data: cryptos, isFetching, isError, error } = useGetCryptosQuery(100);
+  // const {
+  //   data: cryptoExchanges,
+  //   isFetching: isEx,
+  //   isError,
+  //   error,
+  // } = useGetCryptoExchangesQuery();
+  // console.log(cryptoExchanges);
 
+  if (isFetching) {
+    return <div>Loading</div>;
+  }
 
-export default Exchanges
+  if (isError) {
+    return <div>{JSON.stringify(error)}</div>;
+  }
+
+  return (
+    <Col>
+      <Title level={2}>Current Crypto Exchanges</Title>
+      <Col></Col>
+    </Col>
+  );
+};
+
+export default Exchanges;
